58d819cee18bf43cb710c14fa241b0ab15dc62d5
nandasanchit17 <54496814+nandasanchit17@users.noreply.github.com>
2023-01-05 14:21:48 +0530

    Add files via upload

---
+++ prog8.java
@@ -0,0 +1,14 @@
+package com.demo;
+//comparison operators
+public class prog8 {
+    public static void main(String[] args) {
+        System.out.println(10 == 10);
+        System.out.println(12 > 10);
+        System.out.println(13 <= 10);
+        System.out.println(14 != 12);
+
+        boolean check = 15 >= 16;
+        System.out.println(check);
+
+    }
+}
