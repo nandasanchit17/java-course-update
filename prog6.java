1d728ee1386b658557e145dbc57e2ff97a0bcb86
nandasanchit17 <54496814+nandasanchit17@users.noreply.github.com>
2023-01-05 14:13:32 +0530

    Add files via upload

---
+++ prog6.java
@@ -0,0 +1,11 @@
+package com.demo;
+// addition using method
+public class prog6 {
+    private static int add(int x, int y){
+        return x+y;
+    }
+    public static void main(String[] args){
+        System.out.println(add(30,40));
+    }
+
+}
