a747070fc8295f0c6ff98ea8738e57955e17822c
nandasanchit17 <54496814+nandasanchit17@users.noreply.github.com>
2023-01-05 14:33:40 +0530

    Add files via upload

---
+++ prog11.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+//if/else statement
+public class prog11 {
+    public static void main(String[] args) {
+        boolean num = false;
+        if (num) {
+            System.out.println("The value is true");
+        } else {
+            System.out.println("The value is false");
+        }
+        int n1 = 10;
+        if (n1 == 10) n1++;
+        else n1--;
+        System.out.println(n1);
+    }
+}
