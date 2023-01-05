9af755c35795f11bba881998f7da803ac4cb2249
nandasanchit17 <54496814+nandasanchit17@users.noreply.github.com>
2023-01-05 14:24:49 +0530

    Add files via upload

---
+++ prog9.java
@@ -0,0 +1,12 @@
+package com.demo;
+// shorthand reassignment
+public class prog9 {
+    public static void main(String[] args) {
+        int num = 10;
+        num = num + 1;
+        num += 1;
+        System.out.println(num);
+        num *= num;
+        System.out.println(num);
+    }
+}
