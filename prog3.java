dbdf286d5abeb935f95f6df5735bf8904a4e49d8
nandasanchit17 <54496814+nandasanchit17@users.noreply.github.com>
2023-01-05 13:53:08 +0530

    Add files via upload

---
+++ prog3.java
@@ -0,0 +1,11 @@
+package com.demo;
+// String and basic operations
+public class prog3 {
+    public static void main(String[] args) {
+        String str = "Sanchit Nanda";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        System.out.println(str.trim());
+        System.out.println(str.substring(4,7));
+    }
+}
