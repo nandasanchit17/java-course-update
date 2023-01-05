c19eefac1c46ab89217a8ed4fb2a456483af0669
nandasanchit17 <54496814+nandasanchit17@users.noreply.github.com>
2023-01-05 14:17:53 +0530

    Add files via upload

---
+++ prog7.java
@@ -0,0 +1,17 @@
+package com.demo;
+// post/pre increment and decrement operators
+public class prog7 {
+    public static void main(String[] args){
+        int num = 6;
+        System.out.println(num++);
+        System.out.println(num);
+        System.out.println(++num);
+        System.out.println(num);
+
+        System.out.println(num--);
+        System.out.println(num);
+        System.out.println(--num);
+        System.out.println(num);
+
+    }
+}
